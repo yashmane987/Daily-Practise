c1c6c51f644cf0409848fda3d62cfa76ccfc4807
yashmane987 <yashmane4045@gmail.com>
2025-08-17 16:56:00 +0530

    done

--- Express js/Index.js
+++ Express js/Index.js
@@ -1,15 +1,16 @@
 // What is Express js: its a framework of nodejs with the help of express we can create api
 
+const express = require("express"); 
 
-const express = require("express");
+const app = express(); // create an instance of express
 
-const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-res.send("Hello World")
+app.get('/',(req, res) => { //create route for display output.
+  res.send("Hello Express Js");
 });
 
+
 app.listen(port,()=>{
-    console.log(`Example app running ${port}`);
-})
+console.log(`Ex of expree ${port}`)
+});
